9ce6d44cd54e99ee759d9cf4f137c4ab87560471
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2025-03-20 08:34:51 +0900

    __init__.py

--- mypersonaldockeruse/__init__.py
+++ mypersonaldockeruse/__init__.py
@@ -1,1 +1,9 @@
-__all__,__version__=(lambda x:(x,x.pop()))('__main__ install uninstall 0.0.1'.split())
+__all__,__version__=(lambda x:(x,x.pop()))('__main__ install uninstall conf 0.0.1'.split())
+
+try: import builtins as __builtin__
+except: import __builtin__
+
+__builtin__.conf = None
+from conf import img as __img
+__builtin__.default_img = __img
+del __builtin__.conf, __img
